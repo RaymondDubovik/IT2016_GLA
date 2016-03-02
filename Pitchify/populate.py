c68093299df8e0e9cc4b7ca84353b89fce51e4fb
Raymond <raymond.svchost@gmail.com>
2016-03-02 18:50:35 +0000

    populating pitches

--- Pitchify/populate.py
+++ Pitchify/populate.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from pprint import pprint
 
-from Pitchify.models import Company, Investor
+from Pitchify.models import Company, Investor, Pitch
 from django.contrib.auth.models import User
 
 
@@ -14,11 +15,13 @@ class Population():
 
         companies = self.add_companies()
         investors = self.add_investors()
+        pitches = self.add_pitches(companies)
 
-        # todo: stuff;
+        # todo: offers
         return {
             'companies': companies,
             'investors': investors,
+            'pitches': pitches
         }
 
     def add_companies(self):
@@ -29,7 +32,24 @@ class Population():
         return [self.add_investor("investorUser1"),
                 self.add_investor("investorUser2")]
 
-    def add_company(self, username, description):
+    def add_pitches(self, companies):
+        pitches = []
+        i = 0
+        for company in companies:
+            i += 1
+            pitches.append(self.add_pitch(
+                company,
+                title="My awesome pitch number " + i,
+                description="just a very long description here for some random keywords for testing I believe this should do but is too long for a one liner so that might be a problem in code but who cares I definitely don't as you can see about this PEP8 guideline thing because they are only guidelines after all and I don't want to put hardcoded strings across many lines just to comply with the guidelines",
+                youtube_video_id='6p1ypESj6nI',
+                amount_required=10000,
+                total_stocks=150,
+                price_per_stock=100
+            ))
+        return pitches
+
+    @staticmethod
+    def add_company(username, description):
         user, created = User.objects.get_or_create(username=username)
         user.username = username
         user.save()
@@ -41,7 +61,8 @@ class Population():
         company.save()
         return company
 
-    def add_investor(self, username, website_url="", picture=""):
+    @staticmethod
+    def add_investor(username, website_url="", picture=""):
         user, created = User.objects.get_or_create(username=username)  # TODO: no idea, how to handle images
         user.username = username
         user.save()
@@ -50,6 +71,18 @@ class Population():
         investor.user = user
         investor.website = website_url
         investor.picture = picture
+
+    @staticmethod
+    def add_pitch(company, title, description, amount_required, total_stocks, price_per_stock, youtube_video_id='', created=datetime.now()):
+        pitch, created = Pitch.objects.get_or_create(company=company, title=title)
+        pitch.description = description
+        pitch.amount_required = amount_required
+        pitch.total_stocks = total_stocks
+        pitch.price_per_stock = price_per_stock
+        pitch.youtube_video_id = youtube_video_id
+        pitch.created = created
+        pitch.save()
+        return pitch
 
     def truncate(self):
         User.objects.all().delete()  # foreign keys clean objects in all other tables
