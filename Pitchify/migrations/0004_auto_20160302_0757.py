cf78843f46d6b50eb67ef332f66264313250fab1
Raymond <raymond.svchost@gmail.com>
2016-03-02 08:00:15 +0000

    remaining models implemented

---
+++ Pitchify/migrations/0004_auto_20160302_0757.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Pitchify', '0003_auto_20160220_0055'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Offer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('status', models.CharField(max_length=1)),
+                ('message', models.TextField()),
+                ('answer', models.TextField()),
+                ('stock_count', models.IntegerField()),
+                ('price', models.IntegerField()),
+                ('seen', models.BooleanField(default=False)),
+                ('investor_id', models.ForeignKey(to='Pitchify.Investor')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Pitch',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.TextField()),
+                ('youtube_video_id', models.CharField(max_length=11)),
+                ('amount_required', models.IntegerField()),
+                ('created', models.DateTimeField()),
+                ('total_stocks', models.IntegerField()),
+                ('price_per_stock', models.IntegerField()),
+                ('company_id', models.ForeignKey(to='Pitchify.Company')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='offer',
+            name='pitch_id',
+            field=models.ForeignKey(to='Pitchify.Pitch'),
+            preserve_default=True,
+        ),
+    ]
--- Pitchify/models.py
+++ Pitchify/models.py
@@ -25,3 +25,36 @@ class Investor(models.Model):
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
+
+
+class Pitch(models.Model): # foreign company
+    company_id = models.ForeignKey(Company)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    youtube_video_id = models.CharField(max_length=11)
+    amount_required = models.IntegerField()
+    created = models.DateTimeField()
+    total_stocks = models.IntegerField()
+    price_per_stock = models.IntegerField()
+
+class Offer(models.Model):
+    PENDING = 'P'
+    ACCEPTED = 'A'
+    DECLINED = 'D'
+
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (ACCEPTED, 'Accepted'),
+        (DECLINED, 'Declined'),
+    ]
+
+    pitch_id = models.ForeignKey(Pitch)
+    investor_id = models.ForeignKey(Investor)
+    status = models.CharField(max_length=1) # TODO: implement choices
+    message = models.TextField()
+    answer = models.TextField()
+    stock_count = models.IntegerField()
+    price = models.IntegerField()
+    seen = models.BooleanField(default=False)
+
+
