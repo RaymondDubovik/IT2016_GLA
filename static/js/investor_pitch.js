68a3117f8f29eac75b0a4ef2d1584b990ac231c0
Raymond <raymond.svchost@gmail.com>
2016-03-13 02:11:26 +0000

    js now allowing to make an offer

--- static/js/investor_pitch.js
+++ static/js/investor_pitch.js
@@ -2,6 +2,50 @@
  * Created by svchost on 13.03.2016.
  */
 
+$( document ).ready(function() {
+    var $offerSubmit = $('#offerSubmit');
+    var $stocksOffer = $('#stocks_offer');
+    var $stocksPrice = $('#stocks_price');
+    var $stocksMessage = $('#stocks_message');
+    var $offers = $('#offers');
+    var $yourOffers = $('#your_offers');
+    var $yourOffersSuccess = $('#your_offers_success');
+
+    $offerSubmit.click(function (e) {
+        $stocksOffer.val('');
+        $stocksPrice.val('');
+        $stocksMessage.val('');
+        // TODO: jquery here
+
+        var id = 5;
+        var stockCount = 100;
+        var stockPrice = 50;
+        var stockOfferMessage = 'test message here';
+
+        $yourOffers.hide(300);
+        $yourOffersSuccess.show(300);
+
+        $offers.prepend(
+            '<div id="offer_' + id + '" class="alert alert-info">'+
+                '<div class="text-left">'+
+                    '<div class="full-width">'+
+                        '<p>'+
+                            '<span> ' + stockCount + ' stocks for ' + stockPrice + '£</span>'+
+                            '<span class="float-right">Pending<a href="#" onClick="removeOffer(' + id + ')">(Delete)</a></span>'+
+                        '</p>'+
+                    '</div>'+
+                    '<p>' + stockOfferMessage + '</p>'+
+                '</div>'+
+            '</div>'
+        );
+    })
+});
+
+/*
+
+*/
+
+
 function removeOffer(id) {
     BootstrapDialog.show({
         message: 'Warning! This action is not reversible. Do you really want to delete this Offer?',
